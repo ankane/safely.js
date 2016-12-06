f8c2a11a6c012b119d1b9f2cd72091e528451e93
Andrew Kane <acekane1@gmail.com>
2016-12-05 23:55:45 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Safely.js
+
+```javascript
+safely(keepGoingIfThisFunctionFails)
+```
+
+Exceptions are rescued and automatically reported to your favorite reporting service.
+
+In development and test environments, exceptions are raised so you can fix them.
+
+## Contributing
+
+Everyone is encouraged to help improve this project. Here are a few ways you can help:
+
+- [Report bugs](https://github.com/ankane/safely.js/issues)
+- Fix bugs and [submit pull requests](https://github.com/ankane/safely.js/pulls)
+- Write, clarify, or fix documentation
+- Suggest or add new features
---
+++ safely.js
@@ -0,0 +1,47 @@
+/*
+ * Safely.js
+ * Rescue and report exceptions in non-critical code
+ * https://github.com/ankane/safely.js
+ * v0.1.0
+ * MIT License
+ */
+
+/*jslint browser: true, white: true */
+
+(function (window) {
+  'use strict';
+
+  var Safely = {
+    env: "development",
+    raiseEnvs: ["development", "test"],
+    reportExceptionMethod: function (e) {
+      if (window.Rollbar) {
+        window.Rollbar.error(e);
+      }
+    },
+    reportException: function (e) {
+      Safely.reportExceptionMethod(e);
+    }
+  };
+
+  var safely = function (f) {
+    try {
+      f();
+    } catch (e) {
+      var i;
+      for (i = 0; i < Safely.raiseEnvs.length; i++) {
+        if (Safely.env === Safely.raiseEnvs[i]) {
+          throw(e);
+        }
+      }
+      try {
+        Safely.reportException(e);
+      } catch (e2) {
+        console.log("FAIL-SAFE", e2);
+      }
+    }
+  };
+
+  window.Safely = Safely;
+  window.safely = window.yolo = safely;
+}(window));
